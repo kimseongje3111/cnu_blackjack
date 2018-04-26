7b81ec160b76430ed3e448267778c179253517ad
Kim seongje <tjdwp3s@gmail.com>
2018-04-26 21:58:03 +0900

    evaluator_3

--- src/main/java/com/cnu/blackjack/Evaluator.java
+++ src/main/java/com/cnu/blackjack/Evaluator.java
@@ -1,13 +1,19 @@
 package com.cnu.blackjack;
 
 import com.cnu.blackjack.exceptions.PlayerDoesNotExistException;
+import lombok.Data;
 
 import java.util.Map;
 
+@Data
 public class Evaluator {
 
     private Map<String, Player> playerMap;
     private Dealer dealer;
+
+    public Evaluator() {
+        dealer = new Dealer();
+    }
 
     public Evaluator(Map<String, Player> playerMap) {
         this.playerMap = playerMap;
@@ -25,29 +31,34 @@ public class Evaluator {
         });
     }
 
-    private void ScoreCompare(int dealerScore, String name, Player player) {
+    public void ScoreCompare(int dealerScore, String name, Player player) {
         int player_score;
-
-        int temp_score = player.getHand().CardSum();
-        if (temp_score <= 16){
-            player.hitCard();
-            if (player.getHand().CardSum() > 21) {
-                System.out.println(name + " 플레이어 패배! : 카드의 합이 21 초과");
+        if (dealerScore > 21){
+            playerWin(player);
+            System.out.println(name + " 플레이어 승리! : 딜러의 카드 합이 21 초과");
+        }
+        else{
+            int temp_score = player.getHand().CardSum();
+            if (temp_score <= 16){
+                player.hitCard();
+                if (player.getHand().CardSum() > 21) {
+                    System.out.println(name + " 플레이어 패배! : 카드의 합이 21 초과");
+                }
             }
+            player_score = player.getHand().CardSum();
+            if (player_score == 21){
+                playerWin(player);
+                System.out.println(name + " 플레이어 승리! : 블랙잭");
+            }
+            else if (player_score > dealerScore) {
+                playerWin(player);
+                System.out.println(name + " 플레이어 승리! 점수 : " + player_score);
+            }
+            else if (player_score <= dealerScore) {
+                System.out.println(name + " 플레이어 패배! 점수 : " + player_score);
+            }
+            System.out.println(name + " 플레이어 턴종료");
         }
-        player_score = player.getHand().CardSum();
-        if (player_score == 21){
-            playerWin(player);
-            System.out.println(name + " 플레이어 승리! : 블랙잭");
-        }
-        else if (player_score > dealerScore) {
-            playerWin(player);
-            System.out.println(name + " 플레이어 승리! 점수 : " + player_score);
-        }
-        else if (player_score <= dealerScore) {
-            System.out.println(name + " 플레이어 패배! 점수 : " + player_score);
-        }
-        System.out.println(name + " 플레이어 턴종료");
     }
 
     private void playerWin(Player player) {
